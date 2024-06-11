1a5b7c0629d5e1f5a61fe3e1ded65aa55b6793c8
charmisojitra123 <skillqodecharmi@gmail.com>
2024-06-11 13:41:11 +0530

    11june

--- Tailwind/Tasks/tailwind.config.js
+++ Tailwind/Tasks/tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      lineHeight: {
-        'extra-loose': '5',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
