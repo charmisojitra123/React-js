030213317d7d0440efe2fa0bd23f81269b70ac09
charmisojitra123 <skillqodecharmi@gmail.com>
2024-06-11 23:06:36 +0530

    11june

--- Tailwind/Tasks/tailwind.config.js
+++ Tailwind/Tasks/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'demo10-bg-image': "url('/assets/demo3_leaves.jpg')",
+      }
+    },
   },
   plugins: [],
 }
