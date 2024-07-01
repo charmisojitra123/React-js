3e82d31e51cac5c368c4fd4a6bd7b121a6294c4d
charmisojitra123 <skillqodecharmi@gmail.com>
2024-07-01 20:21:08 +0530

    1july

---
+++ Javascript/Task/If Else Statement/scripts.js
@@ -0,0 +1,168 @@
+// Odd or Even
+{
+  // console.log("Check if a number is odd or even ?");
+  // let num = parseInt(prompt("Enter the number :- "));
+  // console.log(num);
+  // if(num % 2 == 0){
+  //     console.log(`${num} is a Even number.`);
+  // }
+  // else{
+  //     console.log(`${num} is a Odd number.`);
+  // }
+}
+
+// Large Number
+{
+//   console.log("Find the larger of two number ?");
+
+//   let a = 14;
+//   let b = 31;
+
+//   if (a > b) {
+//     console.log(`Value of a ${a} is larger than value of b ${b}`);
+//   } else if (a == b) {
+//     console.log(`Value of a ${a} is equal to value of b ${b}`);
+//   } else {
+//     console.log(`Value of b ${b} is larger than value of a ${a}`);
+//   }
+}
+
+// Arithmetic Operations
+{
+//   console.log("Perform arithmetic operations on two numbers");
+
+//   let a = 5;
+//   let b = 2;
+//   let add = a + b;
+//   let sub = a - b;
+//   let multi = a * b;
+//   let exponent = a ** b;
+//   console.log(`Addition of ${a} and ${b} is :- ${add}`);
+//   console.log(`Subtraction of ${a} and ${b} is :- ${sub}`);
+//   console.log(`Multiplication of ${a} and ${b} is :- ${multi}`);
+//   console.log(`Exponent of ${a} and ${b} is :- ${exponent}`);
+}
+
+// Grade for input marks
+{
+//   console.log("Grade for input marks ?");
+
+//   let history = 78;
+//   let civics = 89;
+//   let geography = 92;
+
+//   console.log(`History marks :- ${history}`);
+//   console.log(`Civics marks :- ${civics}`);
+//   console.log(`Geography marks :- ${geography}`);
+
+//   let total = history + civics + geography;
+//   console.log(`Total marks :- ${total}`);
+
+//   let percent = (total / 300) * 100;
+//   console.log(`Percentange :- ${percent}`);
+
+//   if (percent > 80) {
+//     console.log("Grade A");
+//   } else if (percent > 70) {
+//     console.log("Grade B");
+//   } else if (percent > 60) {
+//     console.log("Grade C");
+//   } else if (percent > 40) {
+//     console.log("Grade D");
+//   } else {
+//     console.log("Fail");
+//   }
+}
+
+// Sort three numbers
+{
+//   console.log("Sort Three numbers");
+
+//   let x = 31;
+//   let y = 11;
+//   let z = 14;
+
+//   if(x < y && x < z){
+//     if(y < z){
+//         console.log(`${x} ${y} ${z}`);
+//     }
+//     else{
+//         console.log(`${x} ${z} ${y}`);
+//     }
+//   }
+//   else if(y < x && y < z){
+//     if(x < z){
+//         console.log(`${y} ${x} ${z}`);
+//     }
+//     else{
+//         console.log(`${y} ${z} ${x}`);
+//     }
+//   }
+//   else{
+//     if(y < x){
+//         console.log(`${z} ${y} ${x}`);
+//     }
+//     else{
+//         console.log(`${z} ${x} ${y}`);
+//     }
+//   }
+}
+
+// JAVASCRIPT LOGICAL PROGRAM
+{
+    console.log("Logical Programs");
+    {
+        let a = -9;
+        let b = 3;
+        let c = a * b;
+        console.log(c);
+    }
+
+    {
+        let a = "Value is";
+        let b = 50;
+        console.log(a + " " + b);
+    }
+
+    {
+        let a = 17;
+        let b = 5;
+        let c = a % b;
+        console.log(c);
+    }
+
+    {
+        let a = 5;
+        let b = 17;
+        let c = a % b;
+        console.log(c);
+    }
+
+    {
+        let a = 5;
+        let b = 10;
+        let c = a / b;
+        console.log(c);
+    }
+
+    {
+        let a = 4;
+        let b = 4;
+        let c = a == b;
+        console.log(c);
+    }
+
+    {
+        let a = 4;
+        let b = 5;
+        let c = a != b;
+        console.log(c);
+    }
+
+    {
+        let a = 7;
+        let b = 8;
+        let c = a <= b;
+        console.log(c);
+    }
+}
