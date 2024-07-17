1f1b3d7f60e1d6ff7a51393ce217988cc16b68c6
charmisojitra123 <skillqodecharmi@gmail.com>
2024-07-17 12:22:53 +0530

    17july

---
+++ Javascript/Array High Order Method/script.js
@@ -0,0 +1,72 @@
+
+let data = [10 , 20 , 30 , 40 , 50 , 60 , 70 , 80];
+
+// Flat Method
+// let data = [10 , 20 , 30 , 40 , 50 , [111 , 222 , 333]];
+// console.log(data);
+// console.log(data.flat());
+
+
+// For Each Method
+// let total = 0;
+
+// data.forEach((ele) => {
+//     total += ele;
+// });
+// console.log(total);
+
+
+// Map Method
+// let total = 0;
+
+// let newData = data.map((ele) => {
+//     return ele*2;
+// });
+// console.log(data);
+// console.log(newData);
+
+
+// Filter Method
+// console.log(data.filter((ele) => ele > 40));
+
+
+// Find Method
+// console.log(data.find((ele) => ele < 20));
+
+
+// Find Index Method
+// console.log(data.findIndex((ele) => ele > 30));
+
+
+// Every Method
+// console.log(data.every((ele) => ele <= 600));
+
+
+// Some Method
+// console.log(data.some((ele) => ele < 10));
+
+
+// Reduce Method
+// console.log(data.reduce((total , ele) => total += ele));
+
+
+// Reduce Right Method
+// console.log(data.reduceRight((total , ele) => total += ele));
+
+
+// Entries Method
+// let a = data.entries();
+// console.log(a.next().value);
+// console.log(a.next().value);
+// console.log(a.next().value);
+// console.log(a.next().value);
+// console.log(a.next().value);
+// console.log(a.next().value);
+// console.log(a.next().value);
+// console.log(a.next().value);
+// console.log(a.next().value);
+
+
+// Copy With In Method
+// console.log(data.copyWithin(4 , 1 , 3));
+console.log(data.copyWithin(4 , 1));
